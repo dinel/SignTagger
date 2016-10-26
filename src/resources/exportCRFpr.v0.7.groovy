6d354571940d8ddf52735b727ab262f4f2cc14fe
dinel <dinel@dinel-home.dinel>
2016-10-26 23:10:41 +0100

    Added licence information

--- src/resources/exportCRFpr.v0.7.groovy
+++ src/resources/exportCRFpr.v0.7.groovy
@@ -1,7 +1,30 @@
+/*
+ * Copyright 2016 dinel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import gate.FeatureMap
 
 
-//author idornescu
+/**
+ * Script that prepares the data and runs CRF++ as a classifier. 
+ * Based on code written by Iustin Dornescu in the FIRST project 
+ * (http://first-asd.eu)
+ * 
+ * @author dinel
+ */
+
 //produce mnodes with all necessary features for CRF models
 //train new models
 //use models to make predictions
